3f1db59e687bdabbc3be0943323b16c00ba683af
MUHAMMED SHEREEF S <69725096+shareefshaji@users.noreply.github.com>
2026-04-18 18:02:57 +0300

    Create __init__.py

---
+++ WebStreamer/bot/__init__.py
@@ -0,0 +1,26 @@
+
+
+from pyrogram import Client
+from ..vars import Var
+
+if Var.SECONDARY:
+    PLUGINS=None
+    NO_UPDATES=True
+else:
+    PLUGINS={"root": "WebStreamer/bot/plugins"}
+    NO_UPDATES=None
+
+StreamBot = Client(
+    name="WebStreamer",
+    api_id=Var.API_ID,
+    api_hash=Var.API_HASH,
+    workdir="WebStreamer",
+    plugins=PLUGINS,
+    bot_token=Var.BOT_TOKEN,
+    sleep_threshold=Var.SLEEP_THRESHOLD,
+    workers=Var.WORKERS,
+    no_updates=NO_UPDATES
+)
+
+multi_clients = {}
+work_loads = {}
